1a2092649867d80a131518aee033cc79b7a5f20d
raihan-muhammad <sukasukangoding@gmail.com>
2022-11-03 11:22:05 +0700

    feat: add dialect options ssl in config database

--- config/config.js
+++ config/config.js
@@ -18,6 +18,11 @@ module.exports = {
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
   }
 }
